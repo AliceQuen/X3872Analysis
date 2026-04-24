280d2a68d0a0fa08adacbb8c15fa7dc04323390f
Junkai QIN <junkaiqin@nd-29.hepthu.com>
2026-04-24 10:48:08 +0800

    Committer: Junkai QIN <junkaiqin@nd-29.hepthu.com>  On branch beta1 	Add new parallerization in Merger 	modified:   MergeRootFiles.C

--- MergeRootFiles.C
+++ MergeRootFiles.C
@@ -4,13 +4,13 @@
 
 void MergeRootFiles() {
     auto startTime = std::chrono::high_resolution_clock::now();
-    std::string dataPath = "~/Charmonium";
+    std::string dataPath = "~/runs/JpsiX3872_Result/";
     
     std::cout << "Starting to merge root files from: " << dataPath << std::endl;
     std::vector<std::string> directories;
     FindDirectoriesWithRootFiles(dataPath, directories);
     ROOT::TProcessExecutor pool;
-    auto workItem = [&](const std::string &dirPath) {
+    auto mergeDir = [&](const std::string &dirPath) {
         TChain chain("mkcands/X_data");
         std::string mergedFiles = dirPath + "/*.root";
         chain.Add(mergedFiles.c_str());
@@ -27,7 +27,7 @@ void MergeRootFiles() {
     #if DEBUG == 1
     std::cout << "[INFO] Finishend searching files. Start merging "<< std::endl;
     #endif
-    pool.Map(workItem, directories);
+    pool.Map(mergeDir, directories);
     #if DEBUG == 1
     std::cout << "[INFO] Finishend merging files "<< std::endl;
     #endif
@@ -44,7 +44,7 @@ void MergeRootFiles() {
         }
         yearToDirectories[year].push_back(dirPath);
     }
-    for (auto &entry : yearToDirectories) {
+    auto mergeYear = [&](std::pair<std::string, std::vector<std::string>> entry) {
         auto &year = entry.first;
         auto &dirs = entry.second;
         TChain chain("SourceTree");
@@ -53,14 +53,35 @@ void MergeRootFiles() {
             chain.Add(existingMergedFile.c_str());
         }
         std::string outputFile = year + "_merged.root";
+        std::remove(outputFile.c_str());
         chain.SetName("SourceTree");
         chain.Merge(outputFile.c_str());
         std::cout << "Merged " << dirs.size() << " directories for year " << year << " into " << outputFile << std::endl;
+        return true;
+    };
+    std::vector<std::pair<std::string, std::vector<std::string>>> yearDirVector;
+    for (auto &entry : yearToDirectories) {
+        yearDirVector.push_back(entry);
     }
-    for (const auto &dirPath : directories) {
+    #if DEBUG == 1
+    std::cout << "[INFO] Starting parallel merging by year" << std::endl;
+    #endif
+    pool.Map(mergeYear, yearDirVector);
+    #if DEBUG == 1
+    std::cout << "[INFO] Finished parallel merging by year" << std::endl;
+    #endif
+    auto cleanupMerged = [&](const std::string &dirPath) {
         std::string existingMergedFile = dirPath + "/merged.root";
         std::remove(existingMergedFile.c_str());
-    }
+        return true;
+    };
+    #if DEBUG == 1
+    std::cout << "[INFO] Starting parallel cleanup of intermediate merged files" << std::endl;
+    #endif
+    pool.Map(cleanupMerged, directories);
+    #if DEBUG == 1
+    std::cout << "[INFO] Finished parallel cleanup" << std::endl;
+    #endif
     auto endTime = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::seconds>(endTime - startTime);
     std::cout << "total time taken: " << duration.count() << " s" << std::endl;
